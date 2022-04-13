efbe041c44f5349dfa2e583426b13127ca1f53fc
Sohel Tanjim <tanjim.sohel885@gmail.com>
2022-04-13 22:47:43 +0600

    add resonsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
 
 function App() {
   return (
--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import SendIcon from "@mui/icons-material/Send";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60vh;
@@ -13,11 +14,13 @@ const Container = styled.div`
 const Title = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })}
 `;
 const Description = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })}
 `;
 const InputContainer = styled.div`
   width: 50%;
@@ -26,6 +29,7 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${mobile({ width: "90%" })}
 `;
 const Input = styled.input`
   border: none;
