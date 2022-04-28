156279f06cbfef61a67e76639de13a4a640e9fbb
Sohel <tanjim.sohel885@gmail.com>
2022-04-28 15:43:49 +0600

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import Cart from "./pages/Cart";
 import Product from "./pages/Product";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Cart />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/product/:id">
+            <Product />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/products/:category">
+            <ProductList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -72,6 +72,7 @@ const AddContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ width: "100%" })}
 `;
 const AmountContainer = styled.div`
   display: flex;
